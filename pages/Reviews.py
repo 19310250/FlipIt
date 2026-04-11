6b1b59cbff04f2a1e66d26874a76c639656d8e60
ThePinkUnicorn6 <76398214+ThePinkUnicorn6@users.noreply.github.com>
2026-04-11 18:23:19 +0100

    Changes to scroll bar, still doesn't work

--- pages/Reviews.py
+++ pages/Reviews.py
@@ -35,24 +35,27 @@ class Reviews(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         self.config(bg=Colours.bg)
+        
+        # Create scrollable body
         sidebar = tk.Frame(self, background=Colours.col1)
         sidebar.place(relx=0, rely=0, relwidth=0.16, relheight=1)
 
-        canvas = tk.Canvas(self, bg=Colours.bg)
+        self.canvas = tk.Canvas(self, bg=Colours.bg)
 
-        canvas.place(relx=0.16, rely=0,relwidth=0.84, relheight=1)
-        self.body = tk.Frame(canvas, background=Colours.bg)
-        canvas.bind(
+        self.canvas.place(relx=0.16, rely=0,relwidth=0.84, relheight=1)
+        self.body = tk.Frame(self.canvas, background=Colours.bg)
+        self.canvas.bind(
             "<Configure>",
-            lambda _: canvas.configure(scrollregion=canvas.bbox("all"))
+            lambda _: self.canvas.configure(scrollregion=self.canvas.bbox("all"))
         )
-        canvas.create_window(0, 0, window=self.body)
-        scrollbar = tk.Scrollbar(self, command=canvas.yview)
+        self.canvas.create_window(0, 0, window=self.body)
+        scrollbar = tk.Scrollbar(self, command=self.canvas.yview)
         scrollbar.place(relx=1, rely=0, relheight=1, anchor="ne")
-        canvas.configure(yscrollcommand=scrollbar.set)
-        
-        self.body.columnconfigure(0, weight=1)
+        self.canvas.configure(yscrollcommand=scrollbar.set)
 
+      
+        self.body.columnconfigure(0, weight=1)
+        # Load sidebar
         lblReviews = tk.Label(sidebar, text="Customer Reviews", anchor="center", background=Colours.col1, foreground=Colours.text)
         lblReviews.pack()
         
@@ -63,34 +66,38 @@ class Reviews(tk.Frame):
             for j in range(0, 5-i):
                tk.Label(frmStars, image=self.imgStarIcon, background=Colours.col1).grid(row=i, column=j)
             tk.Label(frmStars, text=f"{i*3}",background=Colours.col1, foreground=Colours.text).grid(row=i, column=5-i)
-
+        # Load Add reviews box and button
         self.txtAddReview = tk.Text(self.body, background=Colours.col2, height=20)
         addPlaceholder(self.txtAddReview, "Share your thoughts...")
         self.txtAddReview.grid(row=0, sticky='new', padx=20)
         self.scale = tk.Scale(self.body, from_=5, to=1)
         self.scale.grid(row=0, column=1, sticky='ns')
-        btnAddReview = tk.Button(self.body, text="Submit review",background=Colours.col3, command=self.addReview)
+        btnAddReview = tk.Button(self.body, text="Submit review",background=Colours.col3, command=self.addReviewButton)
         btnAddReview.grid(row=1, sticky='ew', padx=20, pady=20, columnspan=2)
         self.frmReviews = tk.Frame(self.body, background=Colours.bg)
         self.frmReviews.grid(row=2,columnspan=2)
         self.loadReviews()
+
+    def updateScrollRegion(self):
+        self.canvas.update_idletasks()
         
     def loadReviews(self):
-        for widget in self.frmReviews.winfo_children():
-            widget.destroy()
         for review in reviews:
-            frmReview = tk.Frame(self.frmReviews, background=Colours.bg)
-            frmReview.pack(anchor="nw")
-            frmNameStars = tk.Frame(frmReview,background=Colours.bg)
-            frmNameStars.pack(anchor="nw")
-            tk.Label(frmNameStars, text=review.name, background=Colours.bg, foreground=Colours.text, font=("Arial", 32)).pack(side="left")
-            for i in range(0, review.rating):
-                tk.Label(frmNameStars, image=self.imgStarIcon, background=Colours.bg).pack(side="right")
+            self.addReview(review)
 
-            tk.Label(frmReview,text=review.text, pady=10, background=Colours.bg, foreground=Colours.text, wraplength=1000, justify=tk.LEFT, font=("Arial", 20)).pack(side="bottom")
+    def addReviewButton(self):
+        thisReview = review(self.controller.username, self.scale.get(), self.txtAddReview.get('1.0', 'end-1c'))
+        reviews.append(thisReview)
+        self.addReview(thisReview)
+        
+    def addReview(self, review):
+        frmReview = tk.Frame(self.frmReviews, background=Colours.bg)
+        frmReview.pack(anchor="nw")
+        frmNameStars = tk.Frame(frmReview,background=Colours.bg)
+        frmNameStars.pack(anchor="nw")
+        tk.Label(frmNameStars, text=review.name, background=Colours.bg, foreground=Colours.text, font=("Arial", 32)).pack(side="left")
+        for i in range(0, review.rating):
+            tk.Label(frmNameStars, image=self.imgStarIcon, background=Colours.bg).pack(side="right")
             
-    def addReview(self):
-        reviews.append(review(self.controller.username, self.scale.get(), self.txtAddReview.get('1.0', 'end-1c')))
-        self.loadReviews()
-            
-            
+        tk.Label(frmReview,text=review.text, pady=10, background=Colours.bg, foreground=Colours.text, wraplength=1000, justify=tk.LEFT, font=("Arial", 20)).pack(side="bottom")
+        self.updateScrollRegion()
